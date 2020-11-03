10fc8fe50e759ef21837a6488a52a982b82d7b29
Mahamat Naim <naimdevelopper@gmal.com>
2020-11-03 06:38:40 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lets build tinder clone</h1>
+
+      <Header />
+      {/*TinderCard */}
+      {/*SwipeButton */}
+    </div>
+  );
+}
+
+export default App;
